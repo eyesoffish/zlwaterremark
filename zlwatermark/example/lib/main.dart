3a5aa7719e3fddf3d7bff70b5aa9a5e38f6e2664
zoulin <zoulin@supermonkey.com.cn>
2021-05-19 10:54:54 +0800

    修改项目名称

--- zlwatermark/example/lib/main.dart
+++ zlwatermark/example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:zlwatermark/zlwatermark.dart';
+import 'package:watermark/zlwatermark.dart';
 
 void main() {
   runApp(MyApp());
--- zlwatermark/test/zlwatermark_test.dart
+++ zlwatermark/test/zlwatermark_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:zlwatermark/zlwatermark.dart';
+import 'package:watermark/zlwatermark.dart';
 
 void main() {
   test('adds one to input values', () {
